57ca8f4f7defac551d36858cc22b1adef1a6ccc2
TruongNH <truong02.bp@gmail.com>
2022-05-16 11:59:06 +0700

    add security config

--- src/main/java/com/socical/network/services/UserService.java
+++ src/main/java/com/socical/network/services/UserService.java
@@ -1,8 +1,9 @@
 package com.socical.network.services;
 
 import com.socical.network.data.entities.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
     User create(User user);
 }
--- src/main/java/com/socical/network/services/impl/UserServiceImpl.java
+++ src/main/java/com/socical/network/services/impl/UserServiceImpl.java
@@ -4,6 +4,9 @@ import com.socical.network.data.entities.User;
 import com.socical.network.data.repositories.UserRepository;
 import com.socical.network.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,10 +17,16 @@ import javax.transaction.Transactional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public User create(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
